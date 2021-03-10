a6a5974690f081169eb724d05b2f8e657dab584e
Bruno Vargas <53790911+bruno-v-osorio@users.noreply.github.com>
2021-03-10 08:46:22 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function($){
+
+    "use strict";
+    /*Inicializar nosso contador*/
+    $('.contar').counterUp({
+        time: 1000,
+        delay: 10
+    });
+
+    /*Deslizar para a Seção*/
+    var $doc = $('html,body');
+    $('.scroll-page').click(function(){
+        $doc.animate({
+            scrollTop: $ ($.attr(this,'href')).offset().top
+        },500);
+        return false;
+    });
+
+})(jQuery);
+
